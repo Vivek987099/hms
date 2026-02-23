ff74111bbfc98c7a82a0e9f78e05f3d07723e9ca
Vivek987099 <viveksinghfzd8868965223@gmail.com>
2026-02-23 22:14:03 +0530

    refactor

--- src/api/User.jsx
+++ src/api/User.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const user=   axios.create({
-  baseURL: "https://hms-backend-production-d710.up.railway.app/api/auth/api/user",
+  baseURL: "https://hms-backend-production-d710.up.railway.app/api/user",
 });
 
 
